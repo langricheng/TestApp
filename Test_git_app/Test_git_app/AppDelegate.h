37ce91de84e1a3cbb36a95f7f83fe9bdb84a71f9
langricheng <langricheng@163.com>
2016-08-05 16:26:23 +0800

    第三个

---
+++ Test_git_app/Test_git_app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_git_app
+//
+//  Created by chengwen on 16/8/5.
+//  Copyright © 2016年 Shenzhen Tentinet Technology Co,. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
