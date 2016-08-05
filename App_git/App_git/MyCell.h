1687e868c00eb13c5fb6e5775c81f7face8bba8f
chengwen <langricheng@163.com>
2016-08-05 14:05:38 +0800

    再添加一个cell

---
+++ App_git/App_git/MyCell.h
@@ -0,0 +1,13 @@
+//
+//  MyCell.h
+//  App_git
+//
+//  Created by chengwen on 16/8/5.
+//  Copyright © 2016年 Shenzhen Tentinet Technology Co,. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCell : UITableViewCell
+
+@end
---
+++ App_git/App_git/MyCell.m
@@ -0,0 +1,23 @@
+//
+//  MyCell.m
+//  App_git
+//
+//  Created by chengwen on 16/8/5.
+//  Copyright © 2016年 Shenzhen Tentinet Technology Co,. Ltd. All rights reserved.
+//
+
+#import "MyCell.h"
+
+@implementation MyCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
